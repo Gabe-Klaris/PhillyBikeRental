a673e712e0d03566e25b15317af87db78a4a3bf2
Tubby101 <40404455+Tubby101@users.noreply.github.com>
2021-05-24 16:18:37 -0400

    adding labels to points (new package)

--- mapplotting.py
+++ mapplotting.py
@@ -3,7 +3,8 @@ from urllib.request import Request, urlopen
 import pandas as pd
 import re
 import json
-import pydeck
+import pydeck as pdk
+import plotly.express as px
 stuff = "https://kiosks.bicycletransit.workers.dev/phl"
 req = Request(stuff, headers={'User-Agent': 'Mozilla/5.0'})
 trip_data = pd.read_csv("indego-trips-2021-q1.csv")
@@ -11,6 +12,7 @@ station_data = pd.read_csv("indego-stations-2021-01-01.csv")
 full_data = trip_data.set_index("start_station").join(station_data.set_index("Station_ID"))
 web_byte = urlopen(req).read()
 webpage = web_byte.decode('utf-8')
+stations = list(station_data.loc[station_data.Status == "Active", "Station_Name"])
 lat = []
 lon = []
 longitude = []
@@ -68,6 +70,25 @@ for i in range (145):
 #then find someway to display this ## next to the matching station?
 st.sidebar.markdown("# Pick a location and it will show the bikes available and location")
 selected_answer = st.sidebar.selectbox("# Pick a location", station_data["Station_Name"])
+names = []
+data = []
+for i in stations[1:]:
+    bruh = list(full_data[full_data["Station_Name"] == i].index)
+    bruh1 = list(trip_data.loc[trip_data.start_station == bruh[0], "start_lat"])
+    bruh2 = bruh1[0]
+    latitude.append(bruh2)
+    names.append(i)
+    data.append("yello")
+for i in stations[1:]:
+    bruh = list(full_data[full_data["Station_Name"] == i].index)
+    bruh1 = list(trip_data.loc[trip_data.start_station == bruh[0], "start_lon"])
+    bruh2 = bruh1[0]
+    longitude.append(bruh2)
+stations_coords = list(zip(latitude, longitude,names))
+all_points = pd.DataFrame(
+    stations_coords,
+    columns=['lat', 'lon','station_name']
+)
 if selected_answer == "Virtual Station":
     st.write("No data here: only for drop offs")
 else:
@@ -80,6 +101,19 @@ else:
         coordinates,
         columns = ['lat', 'lon']
         )
-    st.map(single_point)
+    fig = px.scatter_mapbox(all_points, lat="lat", lon="lon", hover_name="station_name", color_discrete_sequence=["fuchsia"], zoom=3, height=300)
+    fig.update_layout(
+    mapbox_style="white-bg",
+    mapbox_layers=[
+        {
+            "below": 'traces',
+            "sourcetype": "raster",
+            "sourceattribution": "United States Geological Survey",
+            "source": [
+                "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"
+            ]
+        }
+      ])
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    st.plotly_chart(figure_or_data=fig, use_container_width=True)
     st.write("There are currently" , name_avaible[selected_answer] , "bikes available at this location")
-#have all dots plotted then plot blue dot
